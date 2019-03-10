c22401af5933ea5d9d638cc7b2f185d5bd31ed24
VeronikaMishkovec <veronikakireenko@gmail.com>
2019-03-10 19:26:14 +0300

    feat: create function getLoveTrianglesCount

--- src/index.js
+++ src/index.js
@@ -3,5 +3,16 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+    // your implementation
+    var result = 0;
+    preferences.unshift(0);
+    for (var i = 1; i < preferences.length; i++) {
+        var k = preferences[i];
+        var n = preferences[k];
+        if (i == preferences[n]) {
+            result++;
+        }
+    }
+    var res = Math.floor(result / 3);
+    return res;
 };
